7286d533f86f51e515c41f50d42634a0ab19d7a2
AVISHKAR2000 <avishkarh2000@gmail.com>
2023-04-20 11:29:29 +0530

    second commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -49,11 +49,7 @@ class DataIngestion:
             raise CustomException(e,sys)
 
 
-## run data Ingestion
 
-if __name__=='__main__':
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
     
 
 
